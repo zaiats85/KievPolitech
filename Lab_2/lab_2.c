afbf959abfd02c204aee7a04a8bedaa7ca2e02d3
zaiats85 <oleg.zaiats@gmail.com>
2019-01-12 17:44:22 +0200

    finished lab_1

--- Lab_2/lab_2.c
+++ Lab_2/lab_2.c
@@ -1,4 +1,31 @@
-//
-// Created by oleg on 04.11.18.
-//
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <math.h>
 
+/*Option 5 zpi-zp82-005*/
+const float A = 2.54;
+const float B = 1.23;
+const float C = -2.14;
+const float D = -0.23;
+
+/* Represent calculation f(x) = 2*cos(a^b) + | arccos(-√c/d) | */
+int main() {
+
+    /*arccos(x) = y; cos(y) = x; 0<=y<=П, |x|<=1;*/
+    float argument = -sqrt(C/D);
+
+    if( fabs(argument) <= 1) {
+
+        printf("f(x) = 2*cos(a^b) + | arccos(-√c/d) | result is %f \n", 2*cos(pow(A, B)) + abs(acos(argument)));
+
+    } else {
+
+        printf("You can not calculate arccos. \nExceptional condition |argument| > 1. \n");
+
+    }
+
+
+/*return value int*/
+    return 0;
+}
--- Lab_example/main.c
+++ Lab_example/main.c
@@ -3,25 +3,18 @@
 
 const int TAB_SIZE = 8;
 
-/*Option 5 zpi-zp82-005*/
-float a = 2.54;
-float b = 1.23;
-float c = -2.14;
-float d = -0.23;
-
-
 int main() {
     char words[80];
 
     printf("Enter a string, and I ll count spaces for u : ");
 
-/*get string from user, cant use gets as it is claimed to be unsafe*/
+    /*get string from user, cant use gets as it is claimed to be unsafe*/
     fgets(words, 80, stdin);
 
-/*assign amount of spaces to 0*/
+    /*assign amount of spaces to 0*/
     int spaces = 0;
 
-/*calc spaces*/
+    /*calc spaces*/
     for (int i = 0; i < strlen(words); i++) {
         switch (words[i]) {
             /*whitespace*/
@@ -43,20 +36,6 @@ int main() {
 /*output*/
     printf("\n Amount of spaces %d\n", spaces);
 
-    char result[50] = "I am calculating\n";
-
-    void calculateTrigonometricStatement(result);
-
 /*return value int*/
     return 0;
-}
-
-char *calculateTrigonometricStatement(char str[]){
-
-    printf("\n a is %f\n", a);
-    printf("\n a is %f\n", b);
-    printf("\n a is %f\n", c);
-    printf("\n a is %f\n", d);
-
-    return str;
 }
