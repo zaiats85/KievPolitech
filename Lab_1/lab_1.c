e3d329282650275f9744669e77e1a4f94e053dbc
zaiats85 <oleg.zaiats@gmail.com>
2019-01-12 17:44:07 +0200

    finished lab_1

---
+++ Lab_1/lab_1.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <math.h>
+
+/*Option 5 zpi-zp82-005*/
+/*const float A = 2.54;
+const float B = 1.23;
+const float C = -2.14;
+const float D = -0.23;*/
+float A;
+float B;
+float C;
+float D;
+
+/* Represent calculation f(x) = 2*cos(a^b) + | arccos(-√c/d) | */
+int main() {
+
+    scanf("%f", &A);
+    scanf("%f", &B);
+    scanf("%f", &C);
+    scanf("%f", &D);
+
+    /*arccos(x) = y; cos(y) = x; 0 < = y <= П, |x|<=1;*/
+    float argument = -sqrt(C/D);
+
+    if( fabs(argument) <= 1) {
+        printf("f(x) = 2*cos(a^b) + | arccos(-√c/d) | result is %f \n", 2*cos(pow(A, B)) + abs(acos(argument)));
+
+    } else {
+        printf("%s\n", "The arccosine of x is defined as the inverse cosine function of x when -1 ≤ x ≤ 1.");
+        printf("You can not calculate arccos of x = %f \nExceptional condition |argument| > 1. \n", argument);
+    }
+
+/*return value int*/
+    return 0;
+}
