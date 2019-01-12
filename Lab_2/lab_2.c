809451d9eb650d79a274b969b0f72e51a5efda69
zaiats85 <oleg.zaiats@gmail.com>
2019-01-13 00:05:03 +0200

    calculate letters and non letters

--- Lab_2/lab_2.c
+++ Lab_2/lab_2.c
@@ -6,7 +6,9 @@
 /*Option 5 zpi-zp82-005*/
 const int k = 35;
 float s;
-float calculateSequence(float s, int k);
+float calculateSequenceForLoop(float s, int k);
+float calculateSequenceWhileLoop(float s, int k);
+float calculateSequenceDoWhileLoop(float s, int k);
 
 /* Represent sequence ∑ = log10(√s * 1/i**2) for i= (1..k)*/
 int main() {
@@ -17,19 +19,46 @@ int main() {
     if( s <= 0) {
         printf("Log10 is only defined for x > 0. Please change your input \n");
     } else {
-        printf("sum is %f. \n", calculateSequence(s, k));
+        printf("sum in forLoop is %f. \n", calculateSequenceForLoop(s, k));
+        printf("sum in whileLoop is %f. \n", calculateSequenceWhileLoop(s, k));
+        printf("sum in doWhileLoop is %f. \n", calculateSequenceDoWhileLoop(s, k));
     }
 
 /*return value int*/
     return 0;
 }
 
-float calculateSequence(float s, int k){
+float calculateSequenceForLoop(float s, int k){
     float result = 0;
 
     for(int i = 1; i < k; i++) {
         result += log10(sqrt(s/pow( i, 2)));
     }
+
+    return result;
+
+}
+
+float calculateSequenceWhileLoop(float s, int k){
+    float result = 0;
+    int i = 1;
+
+    while( i < k ){
+        result += log10(sqrt(s/pow( i++, 2)));
+    }
+
+    return result;
+
+}
+
+float calculateSequenceDoWhileLoop(float s, int k){
+    float result = 0;
+    int i = 1;
+
+    /* do loop execution */
+    do {
+        result += log10(sqrt(s/pow( i++, 2)));
+    } while( i < k );
 
     return result;
 
