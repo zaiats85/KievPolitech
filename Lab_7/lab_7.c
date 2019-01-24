7ff10c1dbaee1422f6ee847549321314eb4fa4be
zaiats85 <zaiats85@mail.ru>
2019-01-24 23:57:32 +0200

    finished 7

--- Lab_7/lab_7.c
+++ Lab_7/lab_7.c
@@ -1,96 +1,89 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdbool.h>
-#include <math.h>
 
-#define MAX 5 //* bounds
+void formMatrix(int **mat, int size);
+void printMatrix(int **mat, int size);
+void isEvenLine(int **mat, int size);
 
-/* Function declaration */
-void formMatrix(int *mat, int size);
-void printMatrix(int *mat, int size);
-void isEvenLine(int *mat, int size);
-
-void isMonoAscOrDesc(int *mat, int size);
+void isMonoAscOrDesc(int **mat, int size);
 bool isMonoAsc(int *arr, int size);
 bool isMonoDesc(int *arr, int size);
 
+// Dynamically Allocate Memory for 2D Array
 int main()
 {
-    int N, i, j, *ptr;
-    scanf("%d", &N);
+    int N = 0; //variable storing rank
 
-    int mat[N][N];
-    ptr = *mat; // or &mat[0][0]
+    printf("\nEnter the number of Row = ");
+    scanf("%d", &N); //Get input for rank
 
-    /*Input elements in matrix using function*/
-    formMatrix((int *)ptr, N);
+    // dynamically create array of pointers of size N
+    int **ptr = (int **)malloc(N * sizeof(int *));
+    if (ptr == NULL)
+    {
+        fprintf(stderr, "Out of memory");
+        exit(0);
+    }
 
-    /*Print resultant array*/
-    printMatrix((int *)ptr, N);
+    formMatrix((int **)ptr, N);
+    printMatrix((int **)ptr, N);
+    isEvenLine((int **)ptr, N);
+    isMonoAscOrDesc((int **)ptr, N);
 
-    isEvenLine((int *)ptr, N);
-    isMonoAscOrDesc((int *)ptr, N);
+    // deallocate memory
+    for (int i = 0; i < N; i++)
+        free(ptr[i]);
+
+    free(ptr);
 
     return 0;
 }
 
-/**
- * Function to accept a two dimensional array and print its elements.
- * @mat     pointer to first element [0][0] of two dimensional int array.
- */
-void printMatrix(int *mat, int size)
+void formMatrix(int **ptr, int N)
+{
+    int i, j;
+
+    // dynamically allocate memory of size N for each row
+    for (int i = 0; i < N; i++)
+    {
+        ptr[i] = (int *)malloc(N * sizeof(int));
+        if (ptr[i] == NULL)
+        {
+            fprintf(stderr, "Out of memory");
+            exit(0);
+        }
+    }
+
+    // assign values to allocated memory
+    for (int i = 0; i < N; i++)
+        for (int j = 0; j < N; j++)
+            *(ptr[i] + j) = rand() % 100;
+}
+
+void printMatrix(int **ptr, int N)
 {
     int i, j;
 
     // Print elements of two dimensional array.
     printf("Elements in 2D array: \n");
-
-    for (i = 0; i < size; i++) //Loop of row
+    for (int i = 0; i < N; i++)
     {
-        for (j = 0; j < size; j++)// Loop for columm
-        {
-            //Read element of 2D array
-            printf("%d ", *((mat + i*size) + j));
+        for (int j = 0; j < N; j++)
+            printf("%d ", *(ptr[i] + j));
 
-        }
         printf("\n");
     }
 }
 
-
-/**
- * Function to add create quadric matrice.
- * @size    length of matrix
- * @matrix  pointer to first element of 2 dimentional matrix.
- */
-void formMatrix(int *mat, int size){
-    int i, j;
-
-    for (i = 0; i < size; i++) //Loop of row
-    {
-        for (j = 0; j < size; j++)// Loop for coloum
-        {
-            //Read element of 2D array
-            *((mat + i*size) + j) = rand() % (100);
-
-        }
-    }
-}
-
-/**
- * Function to test if a matrix contains lines with all even values.
- * @size    length of matrix
- * @matrix    2 dimentional matrix blueprint.
- */
-void isEvenLine(int *mat, int size) {
+void isEvenLine(int **ptr, int N) {
     int j, i;
     bool flag;
 
-    for(i = 0; i < size; i++) {
+    for(i = 0; i < N; i++) {
         int j = 0;
-
-        while(j < size){
-            if(*((mat + i*size) + j)%2 != 0)
+        while(j < N){
+            if(*(ptr[i] + j)%2 != 0)
             {
                 flag = false;
                 break;
@@ -102,44 +95,29 @@ void isEvenLine(int *mat, int size) {
         printf("%d line contains ", i);
         printf("%s\n", flag ? " even values" : " odd values");
     };
-
 }
 
-
-/**
- * Function to test if a matrix contains lines with monotonic sequence, both ascending/descending.
- * @size    length of matrix
- * @matrix    2 dimentional matrix blueprint.
- */
-void isMonoAscOrDesc(int *mat, int size) {
+void isMonoAscOrDesc(int **ptr, int N) {
     int j, i;
     bool flag;
-    int *line;
 
-    for(i = 0; i < size; i++) {
-
+    for(i = 0; i < N; i++) {
         /*Pointer Ariphmetics*/
-        line = mat + i*size;
+        flag = isMonoAsc(ptr[i], N) || isMonoDesc(ptr[i], N);
 
-        flag = isMonoAsc((int *)line, size) || isMonoDesc((int *)line, size);
         printf("%d line contains", i);
         printf("%s\n", flag ? " mono sequence" : " not mono sequence");
     }
 }
 
-/**
- * Function to test if a line contains values which form monotonic sequence ascending.
- * @size    length of matrix
- * @line    single dimensional array repsenting line
- */
 
-bool isMonoAsc(int *line, int size) {
+bool isMonoAsc(int *line, int N) {
     int i = 0;
     bool flag;
 
-    while(i < (size - 1) ) {
+    while(i < N-1) {
 
-        if( *(line + i) > *(line + i + 1)) {
+        if( *(line + i) > *(line + i + 1)){
             flag = false;
             break;
         }
@@ -151,17 +129,14 @@ bool isMonoAsc(int *line, int size) {
     return flag;
 }
 
-/**
- * Function to test if a line contains values which form monotonic sequence descending.
- * @size    length of matrix
- * @line    single dimensional array repsenting line
- */
-bool isMonoDesc(int *line, int size) {
+
+bool isMonoDesc(int *line, int N) {
     int i = 0;
     bool flag;
 
-    while(i < (size - 1)){
-        if(*(line + i) < *(line + i + 1)){
+    while(i < N-1){
+
+        if( *(line + i) < *(line + i + 1)){
             flag = false;
             break;
         }
